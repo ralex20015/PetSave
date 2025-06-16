61870ef995fca8f1c0269307b91e16491e51448e
Alex Robles <lozanoeduardo8777@gmail.com>
2025-06-15 21:32:47 -0600

    Add a data class for handling pagination of animals

--- 03-domain-layer/projects/starter/app/src/main/java/com/realworld/android/petsave/common/domain/model/pagination/PaginatedAnimals.kt
+++ 03-domain-layer/projects/starter/app/src/main/java/com/realworld/android/petsave/common/domain/model/pagination/PaginatedAnimals.kt
@@ -1,4 +1,8 @@
 package com.realworld.android.petsave.common.domain.model.pagination
 
-class PaginatedAnimals {
-}
+import com.realworld.android.petsave.common.domain.model.animal.details.AnimalWithDetails
+
+data class PaginatedAnimals(
+    val animals: List<AnimalWithDetails>,
+    val pagination: Pagination,
+)
