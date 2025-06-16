b3e16d68d42869972dd06d050a449c655aedec0a
Alex Robles <lozanoeduardo8777@gmail.com>
2025-06-15 21:31:46 -0600

    Add animal entity on the domain

--- 03-domain-layer/projects/starter/app/src/main/java/com/realworld/android/petsave/common/domain/model/animal/Animal.kt
+++ 03-domain-layer/projects/starter/app/src/main/java/com/realworld/android/petsave/common/domain/model/animal/Animal.kt
@@ -1,4 +1,13 @@
 package com.realworld.android.petsave.common.domain.model.animal
 
-class Animal {
-}
+import java.time.LocalDateTime
+
+data class Animal(
+    val id: Long,
+    val name: String,
+    val type: String,
+    val media: Media,
+    val tags: List<String>,
+    val adoptionStatus: AdoptionStatus,
+    val publishedAt: LocalDateTime
+)
